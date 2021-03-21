8dcc3f95d6a0ea947cb0b5a2583d14e22888656c
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-03-21 10:44:41 -0400

    logic of the calculator

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
 const showNumbers = document.querySelector('#show-numbers');
 const numbers = document.getElementsByName('numbers');
 const operators = document.getElementsByName('operators');
-const igual = document.getElementsByName('igual');
+const iqual = document.querySelector('#iqual');
+const deleteAll = document.querySelector('#deleteAll');
+
 
 let currentValor ='';
 let oldValor = '';
-let selectOperator = ''; 
+let selectOperator = '';
+let result = '';
+let displayNumbers  = '';
+let countOperators = 0; 
 
 numbers.forEach (element => element.addEventListener('click',() => {
+    
+    
     currentValor= currentValor + element.textContent;
-    screenNumbers(currentValor);
-    
-    
+    displayNumbers = displayNumbers + element.textContent
+    screenNumbers(displayNumbers);
+    displayNumbers = '';    
 }));
+
+iqual.addEventListener('click', total);
 
 operators.forEach(element => {
     element.addEventListener('click', () => {
-        if(oldValor == ''){
-            oldValor = currentValor;
-            currentValor = '';
-            selectOperator = element.textContent; 
+               
+        countOperators = countOperators + 1;
+        if(countOperators >= 2){
+            total();
         }
-        
+        oldValor = currentValor;
+        currentValor = '';
+        selectOperator = element.textContent;    
         screenNumbers(selectOperator);
-
-        console.log(selectOperator)
-        // console.log(currentValor,oldValor)
-        // if(element.textContent == '='){
-            
-        //     switch (selec)
-            
-        // }
         
-    })
+        
+     })
 })
 
+deleteAll.addEventListener('click',reset);
+
+
 function screenNumbers(data){
+     
      showNumbers.placeholder = showNumbers.placeholder + data;
+    
+}
+
+function addNumbers(a,b){
+    return a +b;
+}
+
+function subtractNumbers(a,b){
+    return a-b;
+}
+
+function reset(){
+    return  (currentValor ='',
+     oldValor = '',
+     selectOperator = '',
+     result = '',showNumbers.placeholder='');
+}
+
+function total(){
+    switch(selectOperator){
+        case '+':
+            
+            result = addNumbers(Number(oldValor),Number(currentValor));
+            showNumbers.placeholder = '';
+            screenNumbers(result)
+            currentValor = result.toString();
+            oldValor = '';
+            result= '';
+        break;
+        case '-':
+            result = subtractNumbers(Number(oldValor),Number(currentValor));
+            showNumbers.placeholder = '';
+            screenNumbers(result)
+            currentValor = result.toString();
+            oldValor = '';
+            result= '';
+            console.log(oldValor,currentValor)
+            
+        break;
+    }
 }
